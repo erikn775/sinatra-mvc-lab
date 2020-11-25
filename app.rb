8710add313ad20c1e7bee807e64b68204182b679
erikn775 <erikn775@gmail.com>
2020-11-25 12:07:04 -0700

    commit tree f44feebb81854fd03474a198d8379afef524a75a

--- app.rb
+++ app.rb
@@ -1,4 +1,14 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/' do 
+        erb :user_input
+    end
+
+    post '/piglatinize' do 
+        @latinized_word = params[:word]
+        puts = @latinized_word
+        erb :word_display
+    end
 end
